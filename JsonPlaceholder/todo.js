d0d986e3642aebda611cc0a0816b08d26b99495b
acruz-23 <cruz.581@outlook.com>
2023-06-06 11:30:26 -0400

    Added an error message on the to do page, added a cat facts page.

--- JsonPlaceholder/todo.js
+++ JsonPlaceholder/todo.js
@@ -9,6 +9,16 @@ function displayTask() {
   fetch("http://jsonplaceholder.typicode.com/todos/" + taskID)
     .then((response) => response.json())
     .then((data) => {
+      console.log(data);
+      if (Object.keys(data).length === 0) {
+        document.getElementById("userId").innerHTML = ``;
+        document.getElementById(
+          "taskid"
+        ).innerHTML = `To do ID: ${taskID} is invalid`;
+        document.getElementById("title").innerHTML = ``;
+        document.getElementById("completed").innerHTML = ``;
+        return;
+      }
       document.getElementById("userId").innerHTML = `User ID: ${data.userId}`;
       document.getElementById("taskid").innerHTML = `Id: ${data.id}`;
       document.getElementById("title").innerHTML = `Title: ${data.title}`;
