b8c2655ba9e242febdb3aee4a0b4c3663098d190
acruz-23 <cruz.581@outlook.com>
2023-06-07 11:33:01 -0400

    added styling, converted fetch function to async, added a date next to days of the week

--- Weather/scripts/scripts.js
+++ Weather/scripts/scripts.js
@@ -5,14 +5,7 @@ const cities = [
   { name: "New York City, NY", latitude: 40.6974881, longitude: -73.979681 },
   { name: "Johnstown, PA", latitude: 40.326943, longitude: -78.9186971 },
 ];
-const weatherProp = [
-  "name",
-  "temperature",
-  "temperatureUnit",
-  "windSpeed",
-  "windDirection",
-  "shortForecast",
-];
+const headers = ["Day", "Temperature", "Wind", "Forecast"];
 
 const weatherSelect = document.getElementById("weather");
 const weatherTblEl = document.getElementById("weatherTbl");
@@ -28,7 +21,8 @@ weatherSelect.addEventListener("change", () => {
   const selectedCity = weatherSelect.value;
   if (selectedCity === "choose") return;
   console.log(selectedCity);
-  generateRows(weatherTblEl, weatherProp, selectedCity, cities);
+  generateHeaders(weatherTblEl, headers);
+  generateRows(weatherTblEl, selectedCity, cities);
 });
 
 function generateHeaders(table, headers) {
@@ -36,28 +30,61 @@ function generateHeaders(table, headers) {
   const row = tableHead.insertRow();
   headers.forEach((header) => {
     const cell = row.insertCell();
-    cell.textContent = header;
+    cell.outerHTML = `<th>${header}</th>`;
   });
 }
-function generateRows(table, prop, cityName, citiesArray) {
+async function generateRows(table, cityName, citiesArray) {
   const tableBodyEl = table.createTBody();
   const cityObj = citiesArray.find((city) => city.name === cityName);
-
   const pointsUrl = `https://api.weather.gov/points/${cityObj.latitude},${cityObj.longitude}`;
-  fetch(pointsUrl)
-    .then((response) => response.json())
-    .then((location) => {
-      console.log(location);
-      fetch(location.properties.forecast)
-        .then((response) => response.json())
-        .then((weather) => {
-          weather.properties.periods.forEach((period, i) => {
-            const row = tableBodyEl.insertRow();
-            prop.forEach((property) => {
-              const cell = row.insertCell();
-              cell.textContent = period[property];
-            });
-          });
-        });
-    });
+
+  const govP = await fetch(pointsUrl);
+  const location = await govP.json();
+  const weatherP = await fetch(location.properties.forecast);
+  const locationWeather = await weatherP.json();
+  console.log(locationWeather.properties.periods);
+  locationWeather.properties.periods.forEach((period) => {
+    const periodDate = new Date(period.startTime);
+    const periodMonth = +periodDate.getMonth() + 1;
+    const periodDay = periodDate.getDate();
+    const row = tableBodyEl.insertRow();
+    const timeCell = row.insertCell();
+    const tempCell = row.insertCell();
+    const windCell = row.insertCell();
+    const forecastCell = row.insertCell();
+
+    timeCell.textContent = `${period.name} (${periodMonth}/${periodDay})`;
+    tempCell.textContent =
+      "Temperature " + period.temperature + " " + period.temperatureUnit;
+    windCell.textContent =
+      "Winds " + period.windDirection + " " + period.windSpeed;
+    forecastCell.textContent = period.shortForecast;
+  });
+
+  // fetch(pointsUrl)
+  //   .then((response) => response.json())
+  //   .then((location) => {
+  //     console.log(location);
+  //     fetch(location.properties.forecast)
+  //       .then((response) => response.json())
+  //       .then((weather) => {
+  //         weather.properties.periods.forEach((period) => {
+  //           const row = tableBodyEl.insertRow();
+  //           const timeCell = row.insertCell();
+  //           const tempCell = row.insertCell();
+  //           const windCell = row.insertCell();
+  //           const forecastCell = row.insertCell();
+
+  //           timeCell.textContent = period.name;
+  //           tempCell.textContent =
+  //             "Temperature " +
+  //             period.temperature +
+  //             " " +
+  //             period.temperatureUnit;
+  //           windCell.textContent =
+  //             "Winds " + period.windDirection + " " + period.windSpeed;
+  //           forecastCell.textContent = period.shortForecast;
+  //         });
+  //       });
+  //   });
 }
